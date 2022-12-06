c234a908586ad9b2c4a813a92432b566b91b32a3
Anastasiia <nastylik25@gmail.com>
2022-12-06 14:04:25 +0200

    Main. Create a new Xcode project

---
+++ Fruits/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectOneApp.swift
+//  Fruits
+//
+//  Created by Анастасія on 06.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
