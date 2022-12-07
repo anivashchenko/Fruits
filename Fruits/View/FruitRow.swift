0c018fac8d3d40e6cc2cf329feb411d6db88e927
Anastasiia <nastylik25@gmail.com>
2022-12-07 11:39:07 +0200

    Iss2. Add fruit row with async image, title and desription

--- Fruits/View/FruitRow.swift
+++ Fruits/View/FruitRow.swift
@@ -9,18 +9,41 @@ import SwiftUI
 
 struct FruitRow: View {
     
-    let fruit: Fruit
+    var fruit: Fruit
     
     var body: some View {
         HStack {
-            Image(systemName: "apple.logo")
-            
-            VStack {
-                Text("Title")
-                Text("Description")
+            let url = URL(string: fruit.imageURL)
+
+            AsyncImage(url: url) { phase in
+                switch phase {
+                case .empty:
+                    ProgressView()
+                case .success(let returnedImage):
+                    returnedImage
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 50, height: 50)
+                case .failure:
+                    Image(systemName: "questionmark.square.dashed")
+                        .font(.headline)
+                default:
+                    Image(systemName: "questionmark.square.dashed")
+                        .font(.headline)
+                }
             }
+                
+            VStack {
+                Text(fruit.name)
+                    .font(.headline)
+                    .foregroundColor(.primary)
+                Text(fruit.mainland)
+                    .font(.caption)
+                    .foregroundColor(.secondary)
+            }
+            
+            Spacer()
         }
-        
     }
 }
 
