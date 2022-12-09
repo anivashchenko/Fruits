7696f4e7a5d96b185013d32dcd0d0cf55c89b855
Anastasiia <nastylik25@gmail.com>
2022-12-09 11:39:51 +0200

    Iss4. Rename Fruit.swift to Food.swift

--- Fruits/Model/Food.swift
+++ Fruits/Model/Food.swift
@@ -1,5 +1,5 @@
 //
-//  Fruit.swift
+//  Food.swift
 //  Fruits
 //
 //  Created by Анастасія on 06.12.2022.
@@ -7,13 +7,14 @@
 
 import Foundation
 
-struct Fruit: Decodable, Hashable, Identifiable {
+struct Food: Decodable, Hashable, Identifiable {
     var id: Int
     var name: String
     var description: String
     var imageURL: String
     var mainland: String
     var color: Color
+    var countValue: Int
     
     enum Color: String, Decodable {
         case yellow
