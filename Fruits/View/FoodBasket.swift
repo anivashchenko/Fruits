cb1f6762970972f7ceb9a8800685a9595b5c26fc
Anastasiia <nastylik25@gmail.com>
2022-12-09 11:52:31 +0200

    Add a new file FoodBasket to show the shopping list

---
+++ Fruits/View/FoodBasket.swift
@@ -0,0 +1,20 @@
+//
+//  FoodBasket.swift
+//  Fruits
+//
+//  Created by Анастасія on 08.12.2022.
+//
+
+import SwiftUI
+
+struct FoodBasket: View {
+    var body: some View {
+        Text("Now, it is empty!")
+    }
+}
+
+struct FoodBasket_Previews: PreviewProvider {
+    static var previews: some View {
+        FoodBasket()
+    }
+}
